b7f9bf7282f3a6c539955e19ade4c3e0b3a1ae3f
danielogunrinde <ogunrinde1220@gmail.com>
2023-06-01 20:18:28 -0600

    hello world

---
+++ src/main/java/com/bytesquad/CConnect/cconnectapp/CconnectAppApplication.java
@@ -0,0 +1,13 @@
+package com.bytesquad.CConnect.cconnectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CconnectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CconnectAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytesquad/CConnect/cconnectapp/CconnectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytesquad.CConnect.cconnectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CconnectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
