c2a84afab0708736bfcd5ae591a79d5909d2f96a
danielogunrinde <ogunrinde1220@gmail.com>
2023-07-26 00:04:55 -0600

    final changes

--- src/main/java/com/bytesquad/CConnect/cconnectapp/cconnectcontroller/NotificationController.java
+++ src/main/java/com/bytesquad/CConnect/cconnectapp/cconnectcontroller/NotificationController.java
@@ -1,12 +1,9 @@
 package com.bytesquad.CConnect.cconnectapp.cconnectcontroller;
 
 import com.bytesquad.CConnect.cconnectapp.dtos.NotificationDto;
-import com.bytesquad.CConnect.cconnectapp.dtos.StaffRegistrationDto;
-import com.bytesquad.CConnect.cconnectapp.dtos.staff.StaffDto;
-import com.bytesquad.CConnect.cconnectapp.dtos.user.UserDto;
-import com.bytesquad.CConnect.cconnectapp.entity.Notification;
 import com.bytesquad.CConnect.cconnectapp.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,17 +16,17 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     @PostMapping("/create-notification")
-    public NotificationDto createNotification(@RequestBody NotificationDto notificationDto){
+    public NotificationDto createNotification(@RequestBody NotificationDto notificationDto) {
         return notificationService.createNotification(notificationDto);
     }
 
-    @PutMapping(value = "/update-notification/{appointmentId}",params = "toStaff")
-    public NotificationDto updateNotification(@PathVariable String appointmentId, @RequestBody NotificationDto notificationDto, @RequestParam boolean toStaff){
-        return notificationService.updateNotification(notificationDto,toStaff, appointmentId);
+    @PutMapping(value = "/update-notification/{appointmentId}", params = "toStaff")
+    public ResponseEntity<?> updateNotification(@PathVariable String appointmentId, @RequestBody NotificationDto notificationDto, @RequestParam boolean toStaff) {
+        return notificationService.updateNotification(notificationDto, toStaff, appointmentId);
     }
 
     @GetMapping("{userId}")
-    public List<NotificationDto> getNotifications(@PathVariable String userId){
+    public List<NotificationDto> getNotifications(@PathVariable String userId) {
         return notificationService.getUserNotifications(userId);
     }
 }
