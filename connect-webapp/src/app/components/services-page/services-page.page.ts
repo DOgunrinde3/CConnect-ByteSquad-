df78cab42ed15b5934678e1f02081c554dc6abd8
Adi <adirathore2204@gmail.com>
2023-07-27 18:08:53 -0600

    commit

--- connect-webapp/src/app/components/services-page/services-page.page.ts
+++ connect-webapp/src/app/components/services-page/services-page.page.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import {FooterPage} from "../footer/footer.page";
 import {HeaderPage} from "../header/header.page";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-services-page',
@@ -17,6 +18,39 @@ export class ServicesPagePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  showContent1: boolean = false;
+  showContent2: boolean = false;
+  showContent3: boolean = false;
+  showContent4: boolean = false;
+
+
+  viewContent1() {
+
+    this.showContent1 = !this.showContent1;
+  }
+  hideContent1() {
+    this.showContent1 = !this.showContent1;
+  }
+
+  viewContent2() {
+    this.showContent2 = !this.showContent2;
+  }
+  hideContent2() {
+    this.showContent2 = !this.showContent2;
+  }
+  viewContent3() {
+    this.showContent3 = !this.showContent3;
+  }
+  hideContent3() {
+    this.showContent3 = !this.showContent3;
+  }
+  viewContent4() {
+    this.showContent4 = !this.showContent4;
+  }
+  hideContent4() {
+    this.showContent4 = !this.showContent4;
+  }
 }
